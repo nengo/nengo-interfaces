3699996f5e4bbe16e8a1947edf225ec70b9c00ee
p3jawors <p3jawors@gmail.com>
2020-10-20 10:35:26 -0400

    Added tests for airsim interface
    - test with pd controller and simple path planner
    - test comparing feedback of nengo node and direct airsim interface (no
    nengo)

--- nengo_interfaces/airsim.py
+++ nengo_interfaces/airsim.py
@@ -124,12 +124,12 @@ class AirSim(nengo.Process):
         # initialize wind and payload to zero
         zeros = self.Vector3r(0, 0, 0)
         self.client.simSetWind(zeros)
-        self.client.simSetExtForce(zeros)
+        # self.client.simSetExtForce(zeros)
 
     def disconnect(self):
 
         ext_force = self.Vector3r(0.0, 0.0, 0.0)
-        self.client.simSetExtForce(ext_force)
+        # self.client.simSetExtForce(ext_force)
         self.client.simSetWind(ext_force)
 
         self.client.reset()
@@ -151,7 +151,7 @@ class AirSim(nengo.Process):
     def make_step(self, shape_in, shape_out, dt, rng, state):
         """Create the function for the Airsim interfacing Nengo Node."""
 
-        self.connect()
+        # self.connect()
 
         def step(t, x):
             """Takes in PWM commands for 4 motors. Returns the state of the drone
@@ -367,3 +367,79 @@ class AirSim(nengo.Process):
 
         roll = math.asin(b((c3 * s1 + c1 * s2 * s3) / cp))  # flipped
         return [roll, pitch, yaw]
+
+if __name__ == '__main__':
+    import nengo
+    import matplotlib.pyplot as plt
+
+    dt = 0.01
+    time = 3
+    control_u = np.array([6800, 6800, 7000, 6800])
+
+    # RUN THROUGH NENGO NODE
+    interface = AirSim(dt=dt)
+    interface.connect()
+    net = nengo.Network(seed=0)
+
+    with net:
+        interface_node = nengo.Node(interface)
+        u = nengo.Node(lambda x:control_u, size_in=0, size_out=4)
+        nengo.Connection(u, interface_node)
+        state_probe = nengo.Probe(interface_node)
+    sim = nengo.Simulator(net, dt=0.01, seed=0)
+    sim.run(time)
+
+    interface.disconnect()
+
+    # NENGO NODE CLASS WITHOUT USING NENGO
+    interface = AirSim(dt=dt)
+    interface.connect()
+    state = []
+
+    labs = ['x', 'y', 'z', 'dx', 'dy', 'dz', 'a', 'b', 'g', 'da', 'db', 'dg']
+    for ii in range(0, int(time/dt)):
+        output = interface.get_feedback()
+        output = np.hstack((
+            np.hstack((output['position'], output['linear_velocity'])),
+            np.hstack((output['taitbryan'], output['angular_velocity']))
+        ))
+
+        state.append(output)
+        interface.send_pwm_signal(control_u)
+
+    state = np.asarray(state).T
+    interface.disconnect()
+
+    # OLD INTERFACE WITHOUT NENGO
+    # from airsim_interface import airsimInterface
+    # interface = airsimInterface(dt=dt)
+    # interface.connect()
+    # state_old = []
+    #
+    # labs = ['x', 'y', 'z', 'dx', 'dy', 'dz', 'a', 'b', 'g', 'da', 'db', 'dg']
+    # for ii in range(0, int(time/dt)):
+    #     output = interface.get_feedback()
+    #     output = np.hstack((
+    #         np.hstack((output['pos'], output['lin_vel'])),
+    #         np.hstack((output['ori'], output['ang_vel']))
+    #     ))
+    #
+    #     state_old.append(output)
+    #     interface.send_motor_commands(control_u)
+    #
+    # state_old = np.asarray(state_old).T
+    # interface.disconnect()
+    #
+    #
+    #
+    # COMPARE
+    plt.figure()
+    data = sim.data[state_probe].T
+    print(data.shape)
+    for ii in range(0, len(data)):
+        plt.subplot(len(data), 1, ii+1)
+        plt.plot(data[ii], label='%s Node' % labs[ii])
+        plt.plot(state[ii], label='%s Node No Nengo' % labs[ii])
+        # plt.plot(state_old[ii], label='%s Old Airsim' % labs[ii])
+        plt.legend()
+    plt.show()
---
+++ nengo_interfaces/tests/test_airsim_feedback.py
@@ -0,0 +1,84 @@
+# Compares the feedback when using the nengo node vs interface directly
+import numpy as np
+import nengo
+import matplotlib.pyplot as plt
+from nengo_interfaces.airsim import AirSim
+import time
+
+dt = 0.01
+sim_time = 3
+control_u = np.array([6800, 6800, 6810, 6800])
+
+# RUN THROUGH NENGO NODE
+interface = AirSim(dt=dt)
+interface.connect()
+# let the drone settle to the ground
+time.sleep(1)
+net = nengo.Network(seed=0)
+
+with net:
+    interface_node = nengo.Node(interface)
+    u = nengo.Node(lambda x:control_u, size_in=0, size_out=4)
+    nengo.Connection(u, interface_node)
+    state_probe = nengo.Probe(interface_node)
+sim = nengo.Simulator(net, dt=0.01, seed=0)
+sim.run(sim_time)
+
+interface.disconnect()
+
+# NENGO NODE CLASS WITHOUT USING NENGO
+interface = AirSim(dt=dt)
+interface.connect()
+# let the drone settle to the ground
+time.sleep(1)
+state = []
+
+labs = ['x', 'y', 'z', 'dx', 'dy', 'dz', 'a', 'b', 'g', 'da', 'db', 'dg']
+for ii in range(0, int(sim_time/dt)):
+    output = interface.get_feedback()
+    output = np.hstack((
+        np.hstack((output['position'], output['linear_velocity'])),
+        np.hstack((output['taitbryan'], output['angular_velocity']))
+    ))
+
+    state.append(output)
+    interface.send_pwm_signal(control_u)
+
+state = np.asarray(state).T
+interface.disconnect()
+
+# NOTE can add test with old interface if we add it to abr_control?
+# OLD INTERFACE WITHOUT NENGO
+# from airsim_interface import airsimInterface
+# interface = airsimInterface(dt=dt)
+# interface.connect()
+# state_old = []
+#
+# labs = ['x', 'y', 'z', 'dx', 'dy', 'dz', 'a', 'b', 'g', 'da', 'db', 'dg']
+# for ii in range(0, int(sim_time/dt)):
+#     output = interface.get_feedback()
+#     output = np.hstack((
+#         np.hstack((output['pos'], output['lin_vel'])),
+#         np.hstack((output['ori'], output['ang_vel']))
+#     ))
+#
+#     state_old.append(output)
+#     interface.send_motor_commands(control_u)
+#
+# state_old = np.asarray(state_old).T
+# interface.disconnect()
+#
+#
+#
+
+# COMPARE
+plt.figure()
+data = sim.data[state_probe].T
+print(data.shape)
+for ii in range(0, len(data)):
+    plt.subplot(len(data), 1, ii+1)
+    plt.plot(data[ii], label='%s Node' % labs[ii])
+    plt.plot(state[ii], label='%s Node No Nengo' % labs[ii])
+    # plt.plot(state_old[ii], label='%s Old Airsim' % labs[ii])
+    plt.legend()
+plt.show()
